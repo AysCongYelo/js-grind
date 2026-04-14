d05d8bc8dfdceba7d522b654c665293edf9af132
AysCongYelo <pascual.eduardace@gmail.com>
2026-04-14 21:53:52 +0800

    lesson 1 - variables

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let name = "John"; // Task 1 Working with Variables
+let admin = name;
+
+console.log(admin);
+
+let ourPlanetName = "Earth"; //TAsk 2 Giving the right name
+let currentVisitor = "Ays";
+
+console.log(ourPlanetName);
+console.log(currentVisitor);
